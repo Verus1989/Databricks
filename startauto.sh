ad32aba18f2929370f7089cdafa7effa274ee63f
Verus1989 <84651256+Verus1989@users.noreply.github.com>
2021-10-02 02:07:15 +0700

    Update startauto.sh

--- startauto.sh
+++ startauto.sh
@@ -2,9 +2,11 @@
 sudo add-apt-repository ppa:canonical-chromium-builds/stage
 /usr/bin/yes | sudo apt update
 /usr/bin/yes | sudo apt install chromium-browser
-wget https://chromedriver.storage.googleapis.com/93.0.4577.63/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod +x chromedriver && sudo mv chromedriver /usr/local/bin/
+wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod +x chromedriver && sudo mv chromedriver /usr/local/bin/
 pip install --upgrade pip
 pip install sockets
 pip install selenium
 pip install Faker
 pip install decode
+wget....
+python auto.py
